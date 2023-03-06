689b69d82d0f3184e2c6125892d34af616b54b0f
Brent Young <bryoung777@icloud.com>
2023-03-05 16:43:56 -0800

    Attempt 1

--- q2.py
+++ q2.py
@@ -1,6 +1,22 @@
 
 # ******************************
 # Make your Code
+ilist=[]
+while True:
+  uinput = input('Enter input: ')
+  ilist.append(uinput)
+  if uinput == 'stop':
+    break
+  if uinput == 'STOP':
+    break
+  shortest = ilist[0]
+  longest = ilist[0]
+  for i in ilist:
+    if len(i) < len(shortest):
+      shortest = i
+    if len(i) > len(longest):
+      longest = i
+print(longest, shortest)
 # ******************************
 
 
